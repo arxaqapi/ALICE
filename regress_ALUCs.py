813efd1d8365bf6ac4e935026bce0c17d4e40eda
arxaqapi <arxaqapi@protonmail.com>
2026-08-08 22:33:07 +0200

    feat: functionify & added ruff

--- regress_ALUCs.py
+++ regress_ALUCs.py
@@ -5,11 +5,6 @@ import sys
 import numpy as np
 from numpy import genfromtxt
 
-curdir = sys.argv[1]
-
-F = genfromtxt(curdir + "/tmp_data/features/final_feats.txt", delimiter='\t')
-
-
 # Old diarizer (Dec 2019)
 #X_phones = [1.1092, 16.1548, -0.317, -0.0014, 48.7033]
 #X_syls = [0.6267, 8.8197, -0.1579, -0.0010, 23.0796]
@@ -20,17 +15,20 @@ X_phones = [0.6675, 10.0683, -0.2763, -0.0009, 42.7907]
 X_syls = [0.3094, 5.5473, -0.1538, -0.0006, 23.2143]
 X_words = [0.1697, 3.5229, -0.0920, -0.0003, 13.9089]
 
-F = np.column_stack((F,np.ones((F.shape[0],1))))
 
-words_est = np.dot(F,X_words)
-words_est = words_est.clip(min = 0)
-syls_est = np.dot(F,X_syls)
-syls_est = syls_est.clip(min = 0)
-phones_est = np.dot(F,X_phones)
-phones_est = phones_est.clip(min = 0)
+def regress_ALUCs(curdir):
+    F = genfromtxt(curdir + "/tmp_data/features/final_feats.txt", delimiter='\t')
 
-Y = np.column_stack((phones_est,syls_est))
-Y = np.column_stack((Y,words_est))
+    F = np.column_stack((F,np.ones((F.shape[0],1))))
 
+    words_est = np.dot(F,X_words)
+    words_est = words_est.clip(min = 0)
+    syls_est = np.dot(F,X_syls)
+    syls_est = syls_est.clip(min = 0)
+    phones_est = np.dot(F,X_phones)
+    phones_est = phones_est.clip(min = 0)
 
-np.savetxt(curdir + "/tmp_data/features/ALUCs_out_individual_tmp.txt", Y, delimiter="\t",fmt='%0.2f')
+    Y = np.column_stack((phones_est,syls_est))
+    Y = np.column_stack((Y,words_est))
+
+    np.savetxt(curdir + "/tmp_data/features/ALUCs_out_individual_tmp.txt", Y, delimiter="\t",fmt='%0.2f')
--- split_to_utterances.py
+++ split_to_utterances.py
@@ -7,37 +7,37 @@ from scipy.io import wavfile
 import numpy as np
 import librosa
 
-curdir = sys.argv[1]
-
 valid_speakers = ['FEM','MAL']
 
-DATA = []
-with open(curdir + '/output_voice_type_classifier/tmp_data/all.rttm') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        DATA.append(row)
-        line_count += 1
 
-curfile = []
-for k in range(0,len(DATA)):
-    s = DATA[k][0].split()
-    filename = s[1]
-    orig_filename = curdir + '/tmp_data/'+ s[1] +'.wav'
-    speaker = s[7]
-    isvalid = False
-    for ref in valid_speakers:
-        if(ref == speaker):
-            isvalid = True
-    if(curfile != orig_filename): # read .wav if not read already
-        try:
-            fs, data = wavfile.read(orig_filename)
-        except ValueError: # Reading failed. Try with librosa.
-            data, fs = librosa.load(orig_filename,16000)
-        curfile = orig_filename
-    onset = float(s[3])
-    offset = onset+float(s[4])
-    if isvalid:
-        y = data[max(0,round(onset*fs)):min(len(data),round(offset*fs))]
-        new_filename = curdir + '/tmp_data/short/'+ filename + ("_%010.0f" % (onset*10000)) + '_' + ("%010.0f" % (offset*10000)) +'.wav'
-        wavfile.write(new_filename,fs,y)
+def split_to_utterances(curdir):
+    DATA = []
+    with open(curdir + '/output_voice_type_classifier/tmp_data/all.rttm') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            DATA.append(row)
+            line_count += 1
+
+    curfile = []
+    for k in range(0,len(DATA)):
+        s = DATA[k][0].split()
+        filename = s[1]
+        orig_filename = curdir + '/tmp_data/'+ s[1] +'.wav'
+        speaker = s[7]
+        isvalid = False
+        for ref in valid_speakers:
+            if(ref == speaker):
+                isvalid = True
+        if(curfile != orig_filename): # read .wav if not read already
+            try:
+                fs, data = wavfile.read(orig_filename)
+            except ValueError: # Reading failed. Try with librosa.
+                data, fs = librosa.load(orig_filename,16000)
+            curfile = orig_filename
+        onset = float(s[3])
+        offset = onset+float(s[4])
+        if isvalid:
+            y = data[max(0,round(onset*fs)):min(len(data),round(offset*fs))]
+            new_filename = curdir + '/tmp_data/short/'+ filename + ("_%010.0f" % (onset*10000)) + '_' + ("%010.0f" % (offset*10000)) +'.wav'
+            wavfile.write(new_filename,fs,y)
